cc40ceb5997e8fbd5bfc51f819ef2418753a8d9d
email <tamarkaluzny@gmail.com>
2022-07-06 17:04:30 +0300

    removed not relevant modules

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import React, { useState, Component } from "react";
+import React, { useState} from "react";
 import SearchBox from "./SearchBox";
 import Maps from "./Maps";
 import "./index.css"
 
-
-
-
 export default function App() {
- 
     const[selectPosition, setSelectPosition]= useState(null);
-   
     
     return(
-      
-      
       <div className = "searchBox" style = {{ border: "2px solid black", width: "100vw", height: "100vh" }}>
-   
-           <SearchBox selectPosition={selectPosition} setSelectPosition={setSelectPosition}  />  
+           <SearchBox selectPosition={selectPosition} setSelectPosition={setSelectPosition}   />  
      
         <div className="Map" style={{ border: "2px solid black", width: " 100vw", height: "100vh" }}>
-
            <Maps selectPosition={selectPosition} /> 
-
         </div>
-
       </div >);
     }
 
